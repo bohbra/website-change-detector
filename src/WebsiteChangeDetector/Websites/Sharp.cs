5f9a00061215409945ae0c54ac8cbeb3c3d30d6a
Robert Gaede <bobgaede@gmail.com>
2021-03-07 15:27:00 -0800

    Add more sharp urls

--- src/WebsiteChangeDetector/Websites/Sharp.cs
+++ src/WebsiteChangeDetector/Websites/Sharp.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace WebsiteChangeDetector.Websites
@@ -8,32 +9,40 @@ namespace WebsiteChangeDetector.Websites
     {
         private readonly IWebDriver _webDriver;
         private readonly AppSettings _settings;
+        private readonly List<string> _urls = new();
 
-        private const string Url = "https://www.sharp.com/coronavirus/vaccine-volunteers.cfm";
-        private const string SearchText = "Non-medical opportunities are filled. We will add future opportunities as spots open.";
+        private const string SearchText = "More info";
 
         public Sharp(IWebDriver webDriver, AppSettings settings)
         {
             _webDriver = webDriver;
             _settings = settings;
+            _urls.Add("https://www.sharp.com/health-classes/volunteer-registration-grossmont-center-covid-19-vaccine-clinic-2558");
+            _urls.Add("https://www.sharp.com/health-classes/volunteer-registration-chula-vista-center-covid-19-vaccine-clinic-2554");
         }
 
         public async Task<bool> Check()
         {
-            // navigate to page
-            _webDriver.Navigate().GoToUrl(Url);
+            foreach (var url in _urls)
+            {
+                // navigate to page
+                _webDriver.Navigate().GoToUrl(url);
 
-            // sleep after load
-            await Task.Delay(TimeSpan.FromSeconds(_settings.PageLoadDelayInSeconds));
+                // sleep after load
+                await Task.Delay(TimeSpan.FromSeconds(_settings.PageLoadDelayInSeconds));
 
-            // search for text
-            var found = !_webDriver.PageSource.Contains(SearchText);
+                // search for text
+                var found = _webDriver.PageSource.Contains(SearchText);
 
-            // log result
-            Console.WriteLine($"{DateTime.Now}: Sharp search result: {found}");
+                // log result
+                Console.WriteLine($"{DateTime.Now}: Sharp search result: {found} [{url}]");
 
-            // result
-            return found;
+                // result
+                if (found)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
