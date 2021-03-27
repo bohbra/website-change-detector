3b046baf6acdc95a5aa5e2ade71377a43e7fc9ae
bohbra <bobgaede@gmail.com>
2021-03-27 12:00:39 -0700

    Add serilog

--- src/WebsiteChangeDetector/Program.cs
+++ src/WebsiteChangeDetector/Program.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
+using Serilog;
 using WebsiteChangeDetector.Common;
 
 namespace WebsiteChangeDetector
 {
     class Program
     {
+        public const string DefaultConsoleTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {ClassName} - {Message:lj} {NewLine}{Exception}";
+
         static async Task Main(string[] args)
         {
             await CreateHostBuilder(args).Build().RunAsync();
@@ -21,6 +25,15 @@ namespace WebsiteChangeDetector
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddServices(hostContext);
+                })
+                .UseSerilog((context, configureLogger) =>
+                {
+                    configureLogger
+                        .ReadFrom.Configuration(context.Configuration)
+                        .Enrich.WithProperty("EnvironmentName", context.HostingEnvironment.EnvironmentName)
+                        .Enrich.WithProperty("ApplicationName", context.HostingEnvironment.ApplicationName)
+                        .Enrich.WithProperty("HostName", Environment.MachineName)
+                        .WriteTo.Console(outputTemplate: DefaultConsoleTemplate);
                 });
     }
 }
