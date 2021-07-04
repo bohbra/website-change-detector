b0bd1e1a03155274ca5ce3cd5beadcdaabb7b215
Robert Gaede <bobgaede@gmail.com>
2021-07-04 10:36:45 -0700

    Remove database project

--- src/WebsiteChangeDetector.Database/Schemas/BalboaTennisClub/Tables/BlackoutDates.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [BalboaTennisClub].[BlackoutDates]
-(
-	[BlackoutDateTime] DATETIME2 NOT NULL, 
-    [Reservation] BIT NOT NULL DEFAULT 0, 
-    [LastModifiedDateTime] DATETIME2 NOT NULL DEFAULT (sysdatetime())
-)
